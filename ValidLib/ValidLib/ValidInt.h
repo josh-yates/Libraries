c0813ae2b5436d44a39538b50edf9d10a4ab6bdf
Joshua Yates <josht.yates@btinternet.com>
2018-07-06 17:55:11 +0100

    Remade project as a library and added a namespace

---
+++ ValidLib/ValidLib/ValidInt.h
@@ -0,0 +1,16 @@
+#ifndef VALIDINT_H
+#define VALIDINT_H
+
+//Validate an integer
+
+#include <string>
+#include <sstream>
+#include <limits>
+
+namespace valid {
+	bool isInt(const std::string InputString, std::string& ErrMessage);
+
+	bool intInRange(const int InputInt, const int MinVal, const int MaxVal);
+}
+
+#endif
---
+++ ValidLib/ValidLib/ValidLib.h
@@ -0,0 +1,7 @@
+#ifndef VALIDLIB_H
+#define VALIDLIB_H
+
+//include all headers in validlib
+#include "ValidInt.h"
+
+#endif
